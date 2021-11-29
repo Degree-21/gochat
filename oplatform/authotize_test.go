7e8ac8024718b822275d70ebad2a875213ade296
ljj <youseeim666@163.com>
2021-11-29 12:08:47 +0800

    处理测试用例

--- oplatform/authotize_test.go
+++ oplatform/authotize_test.go
@@ -15,8 +15,8 @@ import (
 
 func TestGetPreAuthCode(t *testing.T) {
 	//var EventMsg = "QqzXZAu+dmwtinjQ6ilJd57JpD1XI7gbFU4IfnHc9vZj0l84ZjjGtIB9lZokMkm3xeGcVAoYPTOfNuz10Z6yaKJqzdLo5IFd7G72Jd3bAJladFdd2ZVh8RHIyFRsZ3Np1uIT6rhy89cypSo0txNLAQOJtBsYDG+WnSkD4IhQjM8CgmeF7K5ORWb66dRTFqaFfEbV157DbpJOhgqlLc+OrkqtjAVz2W+IMzHwJ8jvfka2+huvEWpudQ6TroXxArEPIWustZVDoTxkKVT+dJDvjovFym0wO/f4ludEtkcw8So1f9l4SYYle/SkItioLdlvR4kGxlpySTektweVLNKhYQHrGZATyTNH2TxJpRvsBNwdO0OkNddngDW08xAPhPc+3BORwvQZE3VRGSdAOpzYAniSCL9u8G+mAm8tLyqRtPdgMGjYIQtykTkHzn7OUO7JhsqYm5ez7OtOw0PTLe+TVA=="
-	op := New("wxc83d3daa98fe100c","dd8c33e9d4634923f70a77ada891f4f8")
-	op.SetServerConfig("womeibanfale","zhinengxiugainimenle00000000000000000000001","123123")
+	op := New("appid","appsecret")
+	op.SetServerConfig("token","encodingAESKey","123123")
 	err := op.Do(context.Background(),GetPreAuthCode(&PreAuthCode{
 		ComponentAppid:       op.appid,
 		ComponentAccessToken: "111111",
@@ -27,10 +27,10 @@ func TestGetPreAuthCode(t *testing.T) {
 func TestGetApiComponentToken(t *testing.T) {
 	var  j = `{
     "authorization_info": {
-        "authorizer_appid": "wx77182eed6aa0cf1b",
-        "authorizer_access_token": "48_U-CvBSMQ8uFNso9tyC0pm9ZHwIKRyIvbdqFBF1mlxeIPhPcCuNiKiU77oslC92HlhyNCzsa7C0iMIKppfsy0EwTzSqpJJTxzD4UMz0UC_vTZwtzeGZb-I8aMaTf0swjGcpjbclMoGa1NB8VRHSLdAFDVGZ",
+        "authorizer_appid": "wx771847566aa0cf1b",
+        "authorizer_access_token": "48_U-CvBSMQ8uFNso9tyC0pmixjfIKRyIvbdqFBF1mlxeIPhPcCuNiKiU77oslC92HlhyNCzsa7C0iMIKppfsy0EwTzSqpJJTxzD4UMz0UC_vTZwtzeGZb-I8aMaTf0swjGcpjbclMoGa1NB8VRHSLdAFDVGZ",
         "expires_in": 7200,
-        "authorizer_refresh_token": "refreshtoken@@@RwkbT6QlFef-sj3qamJPEq5fpqz7FYaTdzJ9dGaPNOs",
+        "authorizer_refresh_token": "refreshtoken@@@RwkbT6Qiekf-sj3qamJPEq5fpqz7FYaTdzJ9dGaPNOs",
         "func_info": [
             {
                 "funcscope_category": {
--- oplatform/oplatform_test.go
+++ oplatform/oplatform_test.go
@@ -15,8 +15,8 @@ import (
 
 func TestOplatform_DecryptEventMessage(t *testing.T) {
 	var EventMsg = "lr/RV+4U21T5Ie+cP+NvRW7lJNLgXD+fVy7iZOSCnKagLdjKHoT5s0bv7EmSrOeTsdi13QA7yv1Sf22d/EWUCwF1D12ImCB9+qeaWsLtUUYEZoQLtALhjBZwp4VrSpBCxiw61MKYpuPV7i/qemZ5LjWcA0FwBp4Mce5Gd8zRikEQyCL+ThHf8Zi2wlG2fue+/ly8Xc/h+0MBCfyekp2JKFnpoAtgblbCOiEZDhKc6a8CP0OerEPann1S0Z5ZMQg5rgJeF+lX2Gyxx1QPIRX8Kxup3MqsC09VHa1vtS585QS9NxMSqOF4Ss3GrdcaWp+CLBa/J+Q35hrmChaDsivbeRIIBcx24ncymMOBIL/buyBG9IC8ezRhPFl7MH4q2Se5"
-	op := New("wxc83d3daa98fe100c","dd8c33e9d4634923f70a77ada891f4f8")
-	op.SetServerConfig("womeibanfale","zhinengxiugainimenle00000000000000000000001","123123")
+	op := New("appid","appsecret")
+	op.SetServerConfig("token","encodingAESKey","123123")
 	msg , err := op.DecryptEventMessage("wx77182eed6aa0cf1b",EventMsg)
 	assert.Nil(t, err)
 	assert.Equal(t, wx.WXML{
@@ -32,8 +32,8 @@ func TestOplatform_DecryptEventMessage(t *testing.T) {
 }
 
 func TestOplatform_Reply(t *testing.T) {
-	op := New("wxc83d3daa98fe100c","dd8c33e9d4634923f70a77ada891f4f8")
-	op.SetServerConfig("womeibanfale","zhinengxiugainimenle00000000000000000000001","123123")
+	op := New("appid","appsecret")
+	op.SetServerConfig("token","encodingAESKey","123123")
 	res , err := op.Reply("1111111111","1111111",NewTextReply("121212"))
 	assert.Nil(t,err)
 	fmt.Println(res)
