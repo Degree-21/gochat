33c17f3e8a358ca4b7e03366980a3c219a80543f
daguang <lianghuanan1997@gmail.com>
2022-06-22 15:08:08 +0800

    ✨ update depend from origin

---
+++ oplatform/official_account.go
@@ -0,0 +1,13 @@
+/*
+@Time : 2021/8/20 12:24 下午
+@Author : 21
+@File : official_account
+@Software: GoLand
+*/
+package oplatform
+
+type OfficialAccount struct {
+	appId        string
+	refreshToken string
+	accessToken  string
+}
