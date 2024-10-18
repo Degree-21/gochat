a265caf8481746ac81b09c82c99441e4da393b2a
21 <1192138141@qq.com>
2024-10-18 16:44:57 +0800

    feat: add

--- minip/order.go
+++ minip/order.go
@@ -71,14 +71,21 @@ type Contact struct {
 }
 
 // Order represents the order information for logistics.
+
 type ParamsUploadShippingOrder struct {
-	OrderKey       string          `json:"order_key"`                  // 订单，需要上传物流信息的订单
+	OrderKey       *OrderKey       `json:"order_key"`
 	LogisticsType  int             `json:"logistics_type"`             //物流模式，发货方式枚举值：1、实体物流配送采用快递公司进行实体物流配送形式 2、同城配送 3、虚拟商品，虚拟商品，例如话费充值，点卡等，无实体配送形式 4、用户自提
 	DeliveryMode   int             `json:"delivery_mode"`              //发货模式，发货模式枚举值：1、UNIFIED_DELIVERY（统一发货）2、SPLIT_DELIVERY（分拆发货） 示例值: UNIFIED_DELIVERY
 	IsAllDelivered bool            `json:"is_all_delivered,omitempty"` //分拆发货模式时必填，用于标识分拆发货模式下是否已全部发货完成，只有全部发货完成的情况下才会向用户推送发货完成通知。示例值: true/false
 	ShippingList   []*ShippingInfo `json:"shipping_list"`              // 物流信息列表，发货物流单列表，支持统一发货（单个物流单）和分拆发货（多个物流单）两种模式，多重性: [1, 10]
 	UploadTime     time.Time       `json:"upload_time"`                //上传时间，用于标识请求的先后顺序 示例值: `2022-12-15T13:29:35.120+08:00`
 	Payer          *Payer          `json:"payer"`                      //	支付者，支付者信息
+}
+type OrderKey struct {
+	OrderNumberType int    `json:"order_number_type"`        // 订单单号类型，用于确认需要上传详情的订单。枚举值1，使用下单商户号和商户侧单号；枚举值2，使用微信支付单号。
+	TransactionId   string `json:"transaction_id,omitempty"` // 原支付交易对应的微信订单号
+	MchId           string `json:"mch_id,omitempty"`         //支付下单商户的商户号，由微信支付生成并下发。
+	OutTradeNo      string `json:"out_trade_no,omitempty"`   //支付下单商户的商户号，由微信支付生成并下发。
 }
 
 // Payer represents the payer information.
